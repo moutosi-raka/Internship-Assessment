ad470d34311a28ef784f4ecdbecb425f732c8e51
moutosi-raka <raka29077997@gmail.com>
2022-12-20 23:57:16 +0600

    set up server

---
+++ internship-assessment-server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server running....')
+  })
+  
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+  })
