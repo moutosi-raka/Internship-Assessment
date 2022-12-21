3b74761b6468554f493ff52cd6ff6e9bee3f3d0b
moutosi-raka <raka29077997@gmail.com>
2022-12-21 09:51:57 +0600

    added navbar

--- intership-assessment/src/Shared/Header/Header.js
+++ intership-assessment/src/Shared/Header/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React, { useState } from 'react';
+import {NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
+
+    
     const menuItem = <>
-    <li><Link to='/'>Layout1</Link></li>
-    <li><Link to='/layout2'>layout2</Link></li>
-    <li><Link to='/layout3'>Layout3</Link></li>
+    <li><NavLink to='/'>Layout1</NavLink></li>
+    <li><NavLink to='/layout2'>layout2</NavLink></li>
+    <li><NavLink to='/layout3'>Layout3</NavLink></li>
     
     </>
     return (
@@ -15,13 +19,13 @@ const Header = () => {
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 navbar-start">
+            <ul tabIndex={0} className="menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 navbar-start">
              {menuItem}
             </ul>
           </div>
         </div>
         <div className="hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="nav menu-horizontal px-1">
             {menuItem}
           </ul>
         </div>
--- intership-assessment/src/layout/Main.js
+++ intership-assessment/src/layout/Main.js
@@ -4,7 +4,7 @@ import Header from '../Shared/Header/Header';
 
 const Main = () => {
     return (
-        <div>
+        <div className='w-11/12 mx-auto my-5'>
             <Header></Header>
             <Outlet></Outlet>
         </div>
